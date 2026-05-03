d44a803a7d0e87adfeba4ab8e98e19a53cee52ec
Matt A-W (he/him) <106216+hey-aw@users.noreply.github.com>
2026-05-03 10:40:29 -0700

    style: use serif navigation titles

--- App/ThatMovieNightLifeIOS/ThatMovieNightLifeIOSApp.swift
+++ App/ThatMovieNightLifeIOS/ThatMovieNightLifeIOSApp.swift
@@ -1,9 +1,14 @@
 import SwiftUI
+import UIKit
 
 @main
 struct ThatMovieNightLifeIOSApp: App {
     @Environment(\.scenePhase) private var scenePhase
     @State private var store = MovieNightStore(platform: .iOS)
+
+    init() {
+        configureNavigationTypography()
+    }
 
     var body: some Scene {
         WindowGroup {
@@ -14,5 +19,30 @@ struct ThatMovieNightLifeIOSApp: App {
                     }
                 }
         }
+    }
+
+    private func configureNavigationTypography() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .black
+        appearance.shadowColor = .clear
+        appearance.largeTitleTextAttributes = [
+            .foregroundColor: UIColor.white,
+            .font: Self.serifFont(size: 34, weight: .bold)
+        ]
+        appearance.titleTextAttributes = [
+            .foregroundColor: UIColor.white,
+            .font: Self.serifFont(size: 17, weight: .semibold)
+        ]
+
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().tintColor = UIColor(AppTheme.Colors.accent)
+    }
+
+    private static func serifFont(size: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let descriptor = UIFont.systemFont(ofSize: size, weight: weight).fontDescriptor
+        return UIFont(descriptor: descriptor.withDesign(.serif) ?? descriptor, size: size)
     }
 }
